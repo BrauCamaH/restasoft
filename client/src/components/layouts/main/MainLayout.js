61e1181e38ef7b0857c89b04c55b53532066a355
BrauCamaH <braulio.camarenah@gmail.com>
2019-09-16 16:47:03 -0500

    Add main layout

---
+++ client/src/components/layouts/main/MainLayout.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+import TopBar from './TopBar';
+import SideBar from './sidebar/SideBar';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
+
+function Main() {
+  const classes = useStyles();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline>
+        <TopBar classes={classes} onDrawerToggle={handleDrawerToggle}></TopBar>
+        <SideBar
+          classes={classes}
+          onDrawerToggle={handleDrawerToggle}
+          mobileOpen={mobileOpen}
+        ></SideBar>
+      </CssBaseline>
+    </div>
+  );
+}
+
+export default Main;
