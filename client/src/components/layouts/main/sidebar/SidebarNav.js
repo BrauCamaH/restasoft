1537d924fbcbdd8cec0a00016a0824968a361b31
BrauCamaH <braulio.camarenah@gmail.com>
2019-09-16 16:45:41 -0500

    Add navbar items

---
+++ client/src/components/layouts/main/sidebar/SidebarNav.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { List, ListItem, Button, colors } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  item: {
+    display: 'flex',
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  button: {
+    color: colors.blueGrey[800],
+    padding: '10px 8px',
+    justifyContent: 'flex-start',
+    textTransform: 'none',
+    letterSpacing: 0,
+    width: '100%',
+    fontWeight: theme.typography.fontWeightMedium,
+  },
+  icon: {
+    color: theme.palette.icon,
+    width: 24,
+    height: 24,
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: theme.spacing(1),
+  },
+  active: {
+    color: theme.palette.primary.main,
+    fontWeight: theme.typography.fontWeightMedium,
+    '& $icon': {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
+
+const SidebarNav = props => {
+  const { pages } = props;
+  const classes = useStyles();
+
+  return (
+    <List>
+      {pages.map(page => (
+        <ListItem key={page.title} className={classes.item}>
+          <Button activeClassName={classes.active} className={classes.button}>
+            <div className={classes.icon}>{page.icon}</div>
+            {page.title}
+          </Button>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default SidebarNav;
