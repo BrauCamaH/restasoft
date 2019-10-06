23da430b4efa7567051842272f1a47c7a2bcec82
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-06 12:30:21 -0500

    Fix addtable

--- server/api/routes/tables.js
+++ server/api/routes/tables.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const TablesController = require('../controllers/tables');
 
 router.get('/', TablesController.getTables);
-router.post('/', TablesController.getTables);
+router.post('/', TablesController.addTable);
 
 router.put('/:id', TablesController.updateTable);
 router.delete('/:id', TablesController.deleteTable);
