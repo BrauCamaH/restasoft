3f59d80d7fe4a55bffa0a40985f459ec94705463
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-06 13:12:02 -0500

    change target to port 8080

--- client/.storybook/middleware.js
+++ client/.storybook/middleware.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware')
 module.exports = function expressMiddleware (router) {
 router.use('/api', proxy({
-target: 'http://localhost:8090',
+target: 'http://localhost:8080',
 changeOrigin: true
 }))
 }
