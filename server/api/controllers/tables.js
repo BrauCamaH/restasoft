0ee0ff73b3add9f44512ffa866a6667cd1450a17
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-08 10:41:23 -0500

    Change tables cotroller

--- server/api/controllers/tables.js
+++ server/api/controllers/tables.js
@@ -1,61 +1,66 @@
-const { pool } = require('../config/config');
+const { pool } = require('../../config/pool');
+const db = require('../../config/sequelize');
+
+const Tables = db.tables;
+
 exports.getTables = (req, res) => {
-  pool.query('SELECT * FROM tables', (error, results) => {
-    if (error) {
-      res.status(500).json({
-        error: err,
-      });
-    }
-    res.status(200).json(results.rows);
-  });
+  Tables.findAll()
+    .then(tables => res.json(tables))
+    .catch(e => res.json({ err: e }));
 };
 
 exports.addTable = (req, res) => {
   const { code, observations } = req.body;
 
-  pool.query(
-    'INSERT INTO Tables (code, observations) VALUES ($1, $2)',
-    [code, observations],
-    error => {
-      if (error) {
-        res
-          .status(400)
-          .json({ status: 'unsucess', message: 'Can not insert table' });
-      } else {
-        res.status(201).json({
-          status: 'Success',
-          message: 'Table Added',
-          description: `Code: ${code}, Observations:${observations}`,
-        });
-      }
-    },
-  );
+  const table = Tables.build({
+    code: code,
+    observations: observations,
+  });
+
+  table
+    .save()
+    .then(table => res.status(200).json(table))
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
 
 exports.updateTable = (req, res) => {
   const id = req.params.id;
   const { code, observations } = req.body;
-  pool.query(
-    'UPDATE Tables SET code = $1, observations = $2 where id = $3',
-    [code, observations, id],
-    (error, results) => {
-      if (error) {
-        res.status(500).json({
-          error: err,
-        });
-      }
-      res.status(200).send(`Table modified with ID: ${id}`);
+  Tables.update(
+    {
+      code: code,
+      observations: observations,
     },
-  );
-};
-exports.deleteTable = (req, res) => {
-  const id = req.params.id;
-  pool.query('DELETE FROM Tables WHERE id = $1', [id], (error, results) => {
-    if (error) {
+    {
+      where: {
+        id: id,
+      },
+    },
+  )
+    .then(() => {
+      res.status(200).send(`Table updated with ID: ${id}`);
+    })
+    .catch(err => {
       res.status(500).json({
         error: err,
       });
-    }
-    res.status(200).send(`Table deleted with ID: ${id}`);
-  });
+    });
+};
+exports.deleteTable = (req, res) => {
+  const id = req.params.id;
+  Tables.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => res.status(200).send(`Table deleted with ID: ${id}`))
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 };
