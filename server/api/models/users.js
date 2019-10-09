4a5c3242d1bf45ca63ae835649a6501ea6d6551a
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-09 09:24:53 -0500

    Add users model

---
+++ server/api/models/users.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return users;
+};
