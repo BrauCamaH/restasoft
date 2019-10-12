f4d4b9a8bb2429f50b7f0d8a66b8fae8c5588827
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-12 14:51:33 -0500

    Add error handling

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 const tableRoutes = require('./api/routes/tables');
 const usersRoutes = require('./api/routes/users');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -14,6 +16,21 @@ app.use(cors());
 //Routes
 app.use('/api/tables', tableRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 // Start server
 const PORT = process.env.PORT || 8080;
