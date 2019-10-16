b6e72f3a70ae425914174b4e28a7f92849e98998
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-16 10:58:46 -0500

    Allow singin

--- client/src/components/views/SignIn/SingIn.js
+++ client/src/components/views/SignIn/SingIn.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import { Link as RouterLink , withRouter } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
+import React, { useState, useEffect, forwardRef } from 'react';
+
+import { Link as RouterLink, withRouter } from 'react-router-dom';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+
+import axios from 'axios';
+import validate from 'validate.js';
+
+import {
+  TextField,
+  CssBaseline,
+  Avatar,
+  Button,
+  Typography,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  Container,
+  Link,
+} from '@material-ui/core';
+
+const schema = {
+  username: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 20,
+    },
+  },
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 30,
+    },
+  },
+};
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -20,7 +42,8 @@ const useStyles = makeStyles(theme => ({
     },
   },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -30,18 +53,73 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.primary.main,
   },
   form: {
-    width: '100%', 
+    width: '100%',
     marginTop: theme.spacing(1),
   },
   submit: {
-    background : theme.palette.primary.contrastText,
-    color : theme.palette.primary.main,
+    background: theme.palette.primary.contrastText,
+    color: theme.palette.primary.main,
     margin: theme.spacing(3, 0, 2),
   },
 }));
 
 const SignIn = () => {
   const classes = useStyles();
+
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {},
+    touched: {},
+    errors: {},
+  });
+
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+
+  const handleChange = event => {
+    event.persist();
+
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]: event.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true,
+      },
+    }));
+  };
+
+  const [values, setValues] = React.useState({
+    type: 'Waiter',
+  });
+  const hasError = field =>
+    formState.touched[field] && formState.errors[field] ? true : false;
+
+  const handleSubmit = () => {
+    //create user
+    const { username, password } = formState.values;
+    axios
+      .post(`api/users/sign-in`, {
+        username: username,
+        password: password,
+      })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
 
   return (
     <Container component='main' maxWidth='xs'>
@@ -54,43 +132,54 @@ const SignIn = () => {
           Sign in
         </Typography>
         <form className={classes.form}>
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            id='username'
-            label='Username'
-            name='username'
-            autoComplete='username'
-            autoFocus
-          />
-          <TextField
-            variant='outlined'
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            autoComplete='current-password'
-          />
+          <Grid item xs={12}>
+            <TextField
+              error={hasError('username')}
+              fullWidth
+              helperText={
+                hasError('username') ? formState.errors.username[0] : null
+              }
+              label='Username'
+              name='username'
+              onChange={handleChange}
+              type='text'
+              value={formState.values.username || ''}
+              variant='outlined'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              error={hasError('password')}
+              fullWidth
+              helperText={
+                hasError('password') ? formState.errors.password[0] : null
+              }
+              label='Password'
+              name='password'
+              onChange={handleChange}
+              type='password'
+              value={formState.values.password || ''}
+              variant='outlined'
+              margin='normal'
+            />
+          </Grid>
           <FormControlLabel
             control={<Checkbox value='remember' color='primary' />}
             label='Remember me'
           />
           <Button
-            type='submit'
+            //type='submit'
             fullWidth
             variant='contained'
             className={classes.submit}
+            disabled={!formState.isValid}
+            onClick = {handleSubmit}
           >
             Sign In
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href='#' variant='body2'>
+              <Link href='' variant='body2'>
                 Forgot password?
               </Link>
             </Grid>
