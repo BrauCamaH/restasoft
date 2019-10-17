c8b069493c614e942d7fa982fa6608b68799c3f4
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-17 18:03:59 -0500

    Add route to check auth with token

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const UsersController = require('../controllers/users');
-//const checkAuth = require('../middlewares/auth');
+const checkAuth = require('../middlewares/auth');
 
 router.post('/sign-up', UsersController.signUp);
 
@@ -11,5 +11,9 @@ router.post('/sign-in', UsersController.signIn);
 router.delete('/:id', UsersController.deleteUser);
 
 router.get('/', UsersController.getUsers);
+
+router.get('/checkToken', checkAuth, (req, res) => {
+  res.sendStatus(200);
+});
 
 module.exports = router;
