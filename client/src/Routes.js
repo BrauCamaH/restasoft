d9f710fe89c62af1c7b967b28caf6008e33a6fdc
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-17 18:06:21 -0500

    Protect routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,13 +2,13 @@ import { SingIn, SingUp, Tables, Categories } from './components/views';
 import React from 'react';
 import { Switch, Redirect } from 'react-router-dom';
 
-import { RouteWithLayout } from './components/tools';
+import { RouteWithLayout, WithAuth } from './components/tools';
 import { MinimalLayout, MainLayout } from './components/layouts';
 
 const Routes = () => {
   return (
     <Switch>
-      <Redirect exact from='/' to='/sign-up' />
+      <Redirect exact from='/' to='/sign-in' />
       <RouteWithLayout
         component={SingUp}
         exact
@@ -22,13 +22,13 @@ const Routes = () => {
         path='/sign-in'
       />
       <RouteWithLayout
-        component={Tables}
+        component={WithAuth(Tables)}
         exact
         layout={MainLayout}
         path='/tables'
       />
       <RouteWithLayout
-        component={Categories}
+        component={WithAuth(Categories)}
         exact
         layout={MainLayout}
         path='/products'
