f54f5c1b1d74c70a9d9b61a0a60c5fa647ea311a
BrauCamaH <braulio.camarenah@gmail.com>
2019-10-23 08:14:50 -0500

    Validate update password

--- client/src/components/views/Account/components/Settings/Settings.js
+++ client/src/components/views/Account/components/Settings/Settings.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import validate from 'validate.js';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/styles';
 import {
@@ -12,6 +13,22 @@ import {
   TextField,
 } from '@material-ui/core';
 
+const schema = {
+  password: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 32,
+      minimum: 6,
+    },
+  },
+  confirm: {
+    presence: { allowEmpty: false, message: 'is required' },
+    length: {
+      maximum: 32,
+    },
+  },
+};
+
 const useStyles = makeStyles(() => ({
   root: {},
 }));
@@ -21,17 +38,45 @@ const Password = props => {
 
   const classes = useStyles();
 
-  const [values, setValues] = useState({
-    password: '',
-    confirm: '',
+  const [formState, setFormState] = useState({
+    isValid: false,
+    values: {
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: '',
+      phone: '',
+    },
+    touched: {},
+    errors: {},
   });
+  useEffect(() => {
+    const errors = validate(formState.values, schema);
+
+    setFormState(formState => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
 
   const handleChange = event => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
+    event.persist();
+
+    setFormState(formState => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]: event.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true,
+      },
+    }));
   };
+  const hasError = field =>
+    formState.touched[field] && formState.errors[field] ? true : false;
 
   return (
     <Card {...rest} className={clsx(classes.root, className)}>
@@ -40,28 +85,40 @@ const Password = props => {
         <Divider />
         <CardContent>
           <TextField
+            error={hasError('password')}
             fullWidth
+            helperText={
+              hasError('password') ? formState.errors.password[0] : null
+            }
             label='Password'
             name='password'
             onChange={handleChange}
             type='password'
-            value={values.password}
+            value={formState.values.password}
             variant='outlined'
           />
           <TextField
+            error={hasError('confirm')}
             fullWidth
+            helperText={
+              hasError('confirm') ? formState.errors.confirm[0] : null
+            }
             label='Confirm password'
             name='confirm'
             onChange={handleChange}
             style={{ marginTop: '1rem' }}
             type='password'
-            value={values.confirm}
+            value={formState.values.confirm}
             variant='outlined'
           />
         </CardContent>
         <Divider />
         <CardActions>
-          <Button color='primary' variant='outlined'>
+          <Button
+            color='primary'
+            variant='outlined'
+            disabled={!formState.isValid}
+          >
             Update
           </Button>
         </CardActions>
