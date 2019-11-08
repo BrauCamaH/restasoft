2d1855510ba645cb9c6156f8440578630b14d639
BrauCamaH <braulio.camarenah@gmail.com>
2019-11-08 10:26:46 -0600

    Allow export dialogs from tools

--- client/src/components/tools/index.js
+++ client/src/components/tools/index.js
@@ -3,3 +3,5 @@ export { default as WithAuth } from './WithAuth';
 export { default as SearchBar } from './SearchBar';
 export { default as NumberFormatCustom } from './NumberFormatCustom';
 export { default as TextMaskCustom } from './TextMaskCustom';
+export { default as AlertDialog } from './AlertDialog';
+export { default as FormDialog } from './FormDialog';
