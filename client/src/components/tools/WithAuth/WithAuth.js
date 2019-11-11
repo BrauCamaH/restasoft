3daa3e382ce5ac26067c5240f1490205f4d6fb5e
BrauCamaH <braulio.camarenah@gmail.com>
2019-11-11 12:35:22 -0600

    Fix problem with useeffect

--- client/src/components/tools/WithAuth/WithAuth.js
+++ client/src/components/tools/WithAuth/WithAuth.js
@@ -1,42 +1,23 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import UserContext from '../../../context/user-context';
 
-import axios from 'axios';
+import useAxios from 'axios-hooks';
+
 const WithAuth = props => {
   const { ComponentToProtect, layout: Layout, matchProps } = props;
   const context = useContext(UserContext);
-  const [loading, setLoading] = useState(false);
-  const [redirect, setRedirect] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get('/api/auth/checkToken')
-      .then(res => {
-        if (res.status === 200) {
-          setLoading(false);
-          //console.log(res.data.user.userId);
-          context.setUserId(res.data.user.userId);
-        } else {
-          const error = new Error(res.error);
-          throw error;
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        setLoading(false);
-        setRedirect(true);
-      });
-  }, []);
+  const [{ data, error, loading }] = useAxios(`/api/auth/checkToken`);
+
+  const userId = data !== undefined ? data.user.userId : 1;
+  context.setUserId(userId);
 
   return (
     <React.Fragment>
-      {redirect ? (
+      {error ? (
         <Redirect to='/sign-in' />
-      ) : loading ? (
-        <div></div>
-      ) : (
+      ) : loading ? null : (
         <Layout>
           <ComponentToProtect {...matchProps} />
         </Layout>
