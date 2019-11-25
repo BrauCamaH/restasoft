82d1bb75d9561b87d821c1a2caf4e7ad0087266f
Braulio Camarena <braulio.camarenah@gmail.com>
2019-11-25 16:14:53 -0600

    Add route to search sales

--- server/api/routes/sales.js
+++ server/api/routes/sales.js
@@ -6,6 +6,7 @@ const SalesController = require('../controllers/sales');
 router.get('/', SalesController.getSales);
 router.get('/:user', SalesController.getSalesByUser);
 router.get('/finished/:user', SalesController.getLatestSales);
+router.get('/paginate/filter', SalesController.filterSales);
 router.post('/', SalesController.addSale);
 router.put('/finish/:id', SalesController.finishSale);
 
